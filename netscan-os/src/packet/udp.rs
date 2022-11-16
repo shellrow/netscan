4e70c965645696206b2c8fb37f82cd9a5446191e
shellrow <81893184+shellrow@users.noreply.github.com>
2022-11-16 22:54:03 +0900

    Update udp.rs

--- netscan-os/src/packet/udp.rs
+++ netscan-os/src/packet/udp.rs
@@ -5,7 +5,6 @@ use pnet_packet::udp::MutableUdpPacket;
 pub const UDP_HEADER_LEN: usize = 8;
 
 pub fn build_udp_packet(udp_packet:&mut MutableUdpPacket, src_ip: IpAddr, src_port:u16, dst_ip: IpAddr, dst_port:u16) {
-    udp_packet.set_length(8);
     udp_packet.set_source(src_port);
     udp_packet.set_destination(dst_port);
     udp_packet.set_length(32);
