28c591b8ddc53bd451231735fbb74ed7b9d6502e
shellrow <81893184+shellrow@users.noreply.github.com>
2023-08-19 17:29:25 +0900

    Refactoring async host scan
    np-listener migration

--- src/async_io/unix.rs
+++ src/async_io/unix.rs
@@ -9,14 +9,19 @@ use futures::executor::ThreadPool;
 use futures::stream::{self, StreamExt};
 use futures::task::SpawnExt;
 use futures_lite::{future::FutureExt, io};
+use np_listener::packet::TcpIpFingerprint;
 use pnet_packet::Packet;
 use socket2::{Protocol, SockAddr, Type};
+use std::collections::HashSet;
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
+use np_listener::option::PacketCaptureOptions;
+use np_listener::listener::Listner;
+use np_listener::packet::ip::IpNextLevelProtocol;
 
 async fn build_icmpv4_echo_packet() -> Vec<u8> {
     let mut buf = vec![0; 16];
@@ -289,46 +294,108 @@ pub(crate) async fn scan_hosts(
         }
         _ => return HostScanResult::new(),
     };
-    let interfaces = pnet_datalink::interfaces();
-    let interface = match interfaces
-        .into_iter()
-        .filter(|interface: &pnet_datalink::NetworkInterface| {
-            interface.index == scan_setting.if_index
-        })
-        .next()
-    {
-        Some(interface) => interface,
-        None => return HostScanResult::new(),
-    };
-    let config = pnet_datalink::Config {
-        write_buffer_size: 4096,
-        read_buffer_size: 4096,
-        read_timeout: None,
-        write_timeout: None,
-        channel_type: pnet_datalink::ChannelType::Layer2,
-        bpf_fd_attempts: 1000,
-        linux_fanout: None,
-        promiscuous: false,
-    };
-    let (mut _tx, mut rx) = match pnet_datalink::channel(&interface, config) {
-        Ok(pnet_datalink::Channel::Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unknown channel type"),
-        Err(e) => panic!("Error happened {}", e),
-    };
-    let scan_result: Arc<Mutex<ScanResult>> = Arc::new(Mutex::new(ScanResult::new()));
-    let stop: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
-    let receive_result: Arc<Mutex<ScanResult>> = Arc::clone(&scan_result);
-    let receive_stop: Arc<Mutex<bool>> = Arc::clone(&stop);
-    let receive_setting: ScanSetting = scan_setting.clone();
+
     let executor = ThreadPool::new().unwrap();
-    let future = async move {
-        receiver::receive_packets(&mut rx, receive_setting, &receive_result, &receive_stop).await;
+
+    let mut capture_options: PacketCaptureOptions = PacketCaptureOptions {
+        interface_index: scan_setting.if_index,
+        interface_name: scan_setting.if_name.clone(),
+        src_ips: HashSet::new(),
+        dst_ips: HashSet::new(),
+        src_ports: HashSet::new(),
+        dst_ports: HashSet::new(),
+        ether_types: HashSet::new(),
+        ip_protocols: HashSet::new(),
+        duration: scan_setting.timeout,
+        promiscuous: false,
+        store: true,
+        store_limit: u32::MAX,
     };
-    executor.spawn(future).unwrap();
+    for target in scan_setting.targets.clone() {
+        capture_options.src_ips.insert(target.ip_addr);
+    }
+    match scan_setting.scan_type {
+        ScanType::IcmpPingScan => {
+            capture_options.ip_protocols.insert(IpNextLevelProtocol::Icmp);
+        }
+        ScanType::TcpPingScan => {
+            capture_options.ip_protocols.insert(IpNextLevelProtocol::Tcp);
+            for target in scan_setting.targets.clone() {
+                for port in target.get_ports() {
+                    capture_options.src_ports.insert(port);
+                }
+            }
+        }
+        ScanType::UdpPingScan => {
+            capture_options.ip_protocols.insert(IpNextLevelProtocol::Udp);
+        }
+        _ => {}
+    }
+    let listener: Listner = Listner::new(capture_options);
+    let stop_handle = listener.get_stop_handle();
+    let fingerprints: Arc<Mutex<Vec<TcpIpFingerprint>>> = Arc::new(Mutex::new(vec![]));
+    let receive_fingerprints: Arc<Mutex<Vec<TcpIpFingerprint>>> = Arc::clone(&fingerprints);
+    let future = async move {
+        listener.start();
+        for f in listener.get_fingerprints() {
+            receive_fingerprints.lock().unwrap().push(f);
+        }
+    };
+    let lisner_handle: futures::future::RemoteHandle<()> = executor.spawn_with_handle(future).unwrap();
+    
+    // Wait for listener to start (need fix for better way)
+    thread::sleep(Duration::from_millis(1));
+
+    // Send probe packets
     send_ping_packet(&socket, &scan_setting, ptx).await;
     thread::sleep(scan_setting.wait_time);
-    *stop.lock().unwrap() = true;
-    let result: HostScanResult = scan_result.lock().unwrap().host_scan_result.clone();
+    *stop_handle.lock().unwrap() = true;
+
+    // Wait for listener to complete task
+    lisner_handle.await;
+
+    // Parse fingerprints and store results
+    let mut result: HostScanResult = HostScanResult::new();
+    for f in fingerprints.lock().unwrap().iter() {
+        let mut ports: Vec<PortInfo> = vec![];
+        match scan_setting.scan_type {
+            ScanType::IcmpPingScan => {
+                if f.ip_fingerprint.next_level_protocol != IpNextLevelProtocol::Icmp {
+                    continue;
+                }
+            }
+            ScanType::TcpPingScan => {
+                if f.ip_fingerprint.next_level_protocol != IpNextLevelProtocol::Tcp {
+                    continue;
+                }
+                if let Some(tcp_fingerprint) = &f.tcp_fingerprint {
+                    /* if tcp_fingerprint.flags.contains(&TcpFlagKind::Syn) && tcp_fingerprint.flags.contains(&TcpFlagKind::Ack) {
+                        
+                    } */
+                    let port_info: PortInfo = PortInfo {
+                        port: tcp_fingerprint.destination_port,
+                        status: PortStatus::Open,
+                    };
+                    ports.push(port_info);
+                }
+            }
+            ScanType::UdpPingScan => {
+                if f.ip_fingerprint.next_level_protocol != IpNextLevelProtocol::Udp {
+                    continue;
+                }
+            }
+            _ => {}
+        }
+        let host_info: HostInfo = HostInfo {
+            ip_addr: f.ip_fingerprint.source_ip,
+            host_name: scan_setting.ip_map.get(&f.ip_fingerprint.source_ip).unwrap_or(&String::new()).clone(),
+            ttl: f.ip_fingerprint.ttl,
+            ports: ports,
+        };
+        if !result.hosts.contains(&host_info) {
+            result.hosts.push(host_info);
+        }
+    }
     return result;
 }
 
